c2ed59505b3132b401c5a5f5b554113ce506096c
Chase Farmer <chasefarmer2808@ufl.edu>
2016-04-23 20:22:36 -0400

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name allyArtApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the allyArtApp
+ */
+angular.module('allyArtApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name allyArtApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the allyArtApp
+ */
+angular.module('allyArtApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
