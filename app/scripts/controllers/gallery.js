e5541925ab033ddc9480debc0bc2780ef8606697
Chase Farmer <chasefarmer2808@ufl.edu>
2016-05-28 15:15:27 -0400

    working on gallery

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -8,10 +8,30 @@
  * Controller of the allyArtApp
  */
 angular.module('allyArtApp')
-  .controller('GalleryCtrl', function () {
+  .controller('GalleryCtrl', function ($http, $scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    var vm = this;
+    $scope.message = 'hi';
+    $scope.album = [];
+    var token = 'EAAQDMonnluYBAFV8zuLubW4pPDIrl1pDc8koaYO1gfCYdhwqZAwlWtqC03g1jZBX4H77YvW0ZBDk4nmt2IJrk99VwalUF0OZC11oIBsnZCtuZCP5MinbOM8r7R0ZAM2J6rIOnxLQQDx2feTZCRZCQdb6ZAkmFcMD2Ex00ZD',
+        field = 'fields=photos{images}',
+        FBUrl = 'https://graph.facebook.com/v2.6/130259340354428?access_token=' + token + '&' + field;
+
+    vm.getImgsBySize = function(imgs, size) {
+      for (var i = 0; i < imgs.length; i++) {
+        for (var j = 0; j < imgs[i].images.length; j++) {
+          if (imgs[i].images[j].height === size) {
+            $scope.album.push(imgs[i].images[j]);
+          }
+        }
+      }
+    };
+    $http.get(FBUrl).then(function(response) {
+      vm.getImgsBySize(response.data.photos.data, 480);
+    });
   });
