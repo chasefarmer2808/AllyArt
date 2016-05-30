c807a86fbbb8a49b0f724ff500f077e33fc04311
Chase Farmer <chasefarmer2808@ufl.edu>
2016-05-30 14:23:54 -0400

    image page

--- app/scripts/controllers/photodetail.js
+++ app/scripts/controllers/photodetail.js
@@ -8,7 +8,7 @@
  * Controller of the allyArtApp
  */
 angular.module('allyArtApp')
-  .controller('PhotodetailCtrl', function ($scope, $routeParams) {
+  .controller('PhotodetailCtrl', function ($scope, $routeParams, $http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -17,4 +17,14 @@ angular.module('allyArtApp')
 
     $scope.photo_id = $routeParams.photoId;
 
+    var token = 'EAAQDMonnluYBAFV8zuLubW4pPDIrl1pDc8koaYO1gfCYdhwqZAwlWtqC03g1jZBX4H77YvW0ZBDk4nmt2IJrk99VwalUF0OZC11oIBsnZCtuZCP5MinbOM8r7R0ZAM2J6rIOnxLQQDx2feTZCRZCQdb6ZAkmFcMD2Ex00ZD',
+        field = 'fields=picture, name',
+        id = $scope.photo_id,
+        FBUrl = 'https://graph.facebook.com/v2.6/' + id + '?access_token=' + token + '&' + field;
+
+    $http.get(FBUrl).then(function(response) {
+      console.log(response);
+      $scope.source = response.data.picture;
+      $scope.name = response.data.name;
+    });
   });
