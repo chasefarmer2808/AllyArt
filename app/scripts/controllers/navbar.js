3c99abcb87a7c3c96a4eac8ab191521cd6ee67c2
Chase Farmer <chasefarmer2808@ufl.edu>
2016-06-09 21:28:17 -0400

    restarting

--- app/scripts/controllers/navbar.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('allyArtApp').controller('NavbarController', ['$scope', '$location', function($scope, $location) {
-  $scope.isActive = function(loc) {
-    return loc === $location.path();
-  };
-}]);
